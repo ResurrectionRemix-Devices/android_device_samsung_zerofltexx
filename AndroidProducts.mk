d55c99daa1541e40938fb2befae162761afc4ee4
enesuzun2002 <4lukankalar@gmail.com>
2018-03-18 18:56:40 +0000

    RRify
    Change-Id: I3e6bc91887d8b01eb75c03444dfae22f0e3bab32

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,3 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_zerofltexx.mk \
-    $(LOCAL_DIR)/lineage.mk
+    $(LOCAL_DIR)/rr.mk
--- rr.mk
+++ rr.mk
@@ -4,12 +4,12 @@ $(call inherit-product, device/samsung/zerofltexx/full_zerofltexx.mk)
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Resurrection Remix stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="zerofltexx" \
     TARGET_DEVICE="zeroflte"
 
-PRODUCT_NAME := lineage_zerofltexx
+PRODUCT_NAME := rr_zerofltexx
 PRODUCT_DEVICE := zerofltexx
