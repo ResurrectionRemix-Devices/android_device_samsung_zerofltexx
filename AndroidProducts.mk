6f573eae5ccda42c5b6039f4808cff5de7c621b5
enesuzun2002 <4lukankalar@gmail.com>
2018-04-16 16:00:40 +0000

    zerofltexx: Aosipfy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_zerofltexx.mk \
-    $(LOCAL_DIR)/rr.mk
+    $(LOCAL_DIR)/rr.mk \
+    $(LOCAL_DIR)/aosip.mk
---
+++ aosip.mk
@@ -0,0 +1,15 @@
+# Initialise device config
+$(call inherit-product, device/samsung/zerofltexx/full_zerofltexx.mk)
+
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common Aosip stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="zerofltexx" \
+    TARGET_DEVICE="zeroflte"
+
+PRODUCT_NAME := aosip_zerofltexx
+PRODUCT_DEVICE := zerofltexx
