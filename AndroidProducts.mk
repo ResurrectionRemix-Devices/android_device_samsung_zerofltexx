43959001ab43032c2a74743a8cfb20d99ddca244
enesuzun2002 <4lukankalar@gmail.com>
2018-09-02 04:33:19 +0000

    zerofltexx: Use aosp_zerofltexx to compile Pie

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_zerofltexx.mk \
-    $(LOCAL_DIR)/lineage_zerofltexx.mk 
+    $(LOCAL_DIR)/lineage_zerofltexx.mk \
+    $(LOCAL_DIR)/aosp_zerofltexx.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,3 +15,4 @@
 #
 
 add_lunch_combo lineage_zerofltexx-userdebug
+add_lunch_combo aosp_zerofltexx-userdebug
